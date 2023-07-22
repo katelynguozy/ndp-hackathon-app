bc465767753f7a1a16fab5173dbcd63d0db120a8
katelynguozy <katelynguozy@gmail.com>
2023-07-22 10:15:54 +0800

    Initial Commit

---
+++ ndp hackathon app/ndp_hackathon_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ndp_hackathon_appApp.swift
+//  ndp hackathon app
+//
+//  Created by Katelyn Guo on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ndp_hackathon_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
